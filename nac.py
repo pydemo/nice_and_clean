1506cdeb77ed0ffa3a9dd5e6bb62302f4452bb62
Olek Buzu <olek.buzu@gmail.com>
2020-01-27 10:47:00 -0500

    new

--- nac.py
+++ nac.py
@@ -44,14 +44,16 @@ kws  = ['QlikView', 'Tableau', 'Hadoop Admin', '.Net',  'Hadoop Architect', 'Pow
 'Splunk Developer', 'MongoDB Clustering and Mongo DB scaling', 'Microstrategy Developer','Informatica Developer',
 'Django and Flask', 'JavaScript and HTML5', 'Oracle Cloud CRM Functional Architect', 'Oracle Architect',
 'PowerCenter', 'Content Management Analyst', 'JAVA, Kafka', 'PySpark Expert', 'Hadoop, Map Reduce, Yarn',
-'Administrative Assistant', 'Oracle Fusion Cloud Financials','El Segundo, CA','Java Backend Developer']
+'Administrative Assistant', 'Oracle Fusion Cloud Financials','El Segundo, CA','Java Backend Developer',
+'Security Engineer']
 
 locs = ['Garden City, NY', 'Arizona', 'Washington DC','Albertville, AL', 'Columbus OH', 'Denver, CO', 'Dallas TX',
 'RENTON, Washington','Branchburg, NJ', 'Whippany, NJ', 'Baltimore, MD', 'Phoenix, AZ',
 'St. Paul, MN', 'Renton, WA', 'Providence, RI', 'Westin, NJ','Atlanta, GA', 'Stow, MA', 'Frisco, TX','Dallas, TX',
 'McLean, VA', 'Berwyn, PA', 'Dedham, MA','Louisville', 'Dublin, OH', 'San Ramon,CA', 'San Diego, CA', 'Seattle, WA',
 'Sunnyvale, CA', 'Houston, TX', 'Alpharetta, GA', 'Pittsburgh PA', 'Quincy, MA', 'Dedham, MA', 'Carry, NC',
-'Columbus, OH', 'Chandler, AZ', 'Nashville, TN', 'Charlotte, NC', 'Fremont, CA','Princeton, NJ', 'Charlotte', 'Hillsboro']
+'Columbus, OH', 'Chandler, AZ', 'Nashville, TN', 'Charlotte, NC', 'Fremont, CA','Princeton, NJ', 'Charlotte', 'Hillsboro',
+'Glenivew, IL']
 
 
 
